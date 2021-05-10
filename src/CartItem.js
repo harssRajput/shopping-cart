ff71c2473632797a910c1b06434ead7f2bfbbf4d
Harsh Rajput <hrrajput20@gmail.com>
2021-05-10 15:44:02 +0530

    add style for cartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,39 +4,22 @@ import trashIcon from "./trash.png";
 
 function CartItem() {
   return (
-    <div style= {styles.cartItem} className="cartItem">
-      <div style={styles.leftPart}>
+    <div className="cartItem">
+      <div className="left-block">
         <img src="" alt="" />
       </div>
-      <div>
-        <p>name : phone</p>
-        <p>price : Rs 000</p>
-        <p>qty : 2</p>
-        <img style={styles.imgIcon} src={minusIcon} alt="" />
-        <img style={styles.imgIcon} src={plusIcon} alt="" />
-        <img style={styles.imgIcon} src={trashIcon} alt="" />
+      <div className="right-block">
+        <p style={{ fontWeight: "bold" }}>phone</p>
+        <p style={{ color: "#777" }}>Rs 000</p>
+        <p style={{ color: "#777" }}>qty : 2</p>
+        <div className="action-buttons">
+          <img className="action-button" src={minusIcon} alt="" />
+          <img className="action-button" src={plusIcon} alt="" />
+          <img className="action-button" src={trashIcon} alt="" />
+        </div>
       </div>
     </div>
   );
 }
-
-const styles = {
-  imgIcon: {
-    width: 25,
-    height: 25,
-    padding: 5
-  },
-  leftPart: {
-    marginRight: 5,
-    width: 150,
-    // height: 100,
-    backgroundColor: "grey"
-  },
-  cartItem:{
-    margin :15,
-    // marginBottom : 10,
-    display: "flex"
-  }
-};
 
 export default CartItem;
